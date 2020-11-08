4a70542ec0bb927072833e5f82bf899a516f7600
bibhutighimire <ghimirebibhuti@gmail.com>
2020-11-08 13:14:14 -0700

    Add project files.

---
+++ WebApiUsingCore/Models/Employees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiUsingCore.Models
+{
+    public class Employees
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Name
+        {
+            public string first { get; set; }
+            public string last { get; set; }
+        }
+
+        public class MyArray
+        {
+            public Name name { get; set; }
+            public string jobTitle { get; set; }
+            public string photoURL { get; set; }
+        }
+
+        public class Root
+        {
+            public List<MyArray> MyArray { get; set; }
+        }
+
+    }
+}
