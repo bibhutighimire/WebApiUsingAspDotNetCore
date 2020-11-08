2f49b394cc9e03bc58098a763d9f21a24d2ee115
bibhutighimire <ghimirebibhuti@gmail.com>
2020-11-08 13:19:55 -0700

    added newtonsoftjson and employee controller

---
+++ WebApiUsingCore/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static WebApiUsingCore.Models.Employees;
+
+namespace WebApiUsingCore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string url = string.Format("https://emplistapi-258220.appspot.com");
+            List<MyArray> emp = null;
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                emp = (new JavaScriptSerializer()).Deserialize<List<EmployeeViewModel>>(json);
+
+            }
+            return View(emp);
+        }
+    }
+}
