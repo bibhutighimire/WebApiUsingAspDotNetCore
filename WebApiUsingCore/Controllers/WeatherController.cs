21841625ad0ae9c83b86fbf9f2ad2e72e579ad50
bibhutighimire <ghimirebibhuti@gmail.com>
2020-11-08 15:25:32 -0700

    weather api works

--- WebApiUsingCore/Controllers/WeatherController.cs
+++ WebApiUsingCore/Controllers/WeatherController.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static WebApiUsingCore.Models.Weather;
 
 namespace WebApiUsingCore.Controllers
 {
     public class WeatherController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            
+        string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=edmonton&units=metric&cnt=1&APPID=8113fcc5a7494b0518bd91ef3acc074f");
+
+                   
+        using (WebClient client = new WebClient())
+        {
+            string json = client.DownloadString(url);
+            var jsonstring = JsonConvert.DeserializeObject<Example>(json);   
+            return View(jsonstring);
+        }
+            
+          
         }
     }
 }
